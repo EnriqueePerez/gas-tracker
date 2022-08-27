78b26a607c0f7fe29934c8e8131ad9a0152e8357
Enrique Perez <enrike_084@hotmail.com>
2022-08-26 21:56:16 -0500

    fix: invalid date on CreateGasDischargePage

--- src/helpers/date-helpers.ts
+++ src/helpers/date-helpers.ts
@@ -1,7 +1,7 @@
 export const getCurrentDate = (minutes: string) => {
   const today = new Date();
   const dd = today.getDate();
-  const mm = today.getMonth() + 1;
+  const m = today.getMonth() + 1;
   const yyyy = today.getFullYear();
-  return new Date(`${yyyy}-${mm}-${dd} ${minutes}:00`).toISOString();
+  return new Date(`${yyyy}-0${m}-${dd}T${minutes}:00`).toISOString();
 };
