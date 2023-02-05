6802d9ecb03ea46030b2a93ccc65bbac531bb4d8
EnriqueePerez <enrike_084@hotmail.com>
2023-02-05 16:14:10 -0600

    feat: filter by store on spare listing and colors

--- src/containers/SparesInfoPage/components/SparesListing.tsx
+++ src/containers/SparesInfoPage/components/SparesListing.tsx
@@ -1,6 +1,7 @@
 import {
   Box,
   BoxProps,
+  Input,
   Table,
   TableCaption,
   Tbody,
@@ -8,7 +9,7 @@ import {
   Tr,
 } from '@chakra-ui/react';
 import { nanoid } from 'nanoid';
-import { useCallback } from 'react';
+import { ChangeEvent, useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useUser } from 'reactfire';
 
@@ -29,6 +30,7 @@ export const SparesListing: React.FC<ISparesListingProps> = (
   const { spares = [], ...rest } = props;
 
   const { data: user } = useUser();
+  const [filteredSpares, setFilteredSpares] = useState<ISpare[]>();
 
   const navigate = useNavigate();
 
@@ -42,20 +44,46 @@ export const SparesListing: React.FC<ISparesListingProps> = (
   const setColor = useCallback(
     (spare: ISpare) => {
       let color = 'unset';
-      if (spare.is_delayed === 'Sí') {
-        color = 'red.500';
+      if (!spare.request_date) {
+        color = 'red.300';
+        return color;
+      }
+
+      if (!spare.authorization_date || !spare.reception_date) {
+        color = 'orange.500';
+        return color;
+      }
+
+      if (!spare.installation_date || !spare.service_sheet) {
+        color = 'yellow.300';
+        return color;
       }
 
       if (spare.service_sheet === 'Si') {
-        color = 'green.500';
+        color = 'green.300';
+        return color;
+      }
+
+      if (spare.is_delayed === 'Sí') {
+        color = 'red.500';
+        return color;
       }
       return color;
     },
     [user],
   );
 
+  const handleFilter = (e: ChangeEvent<HTMLInputElement>) => {
+    // filter by store
+    const filtered = spares.filter((spare) =>
+      spare.store.toLowerCase().includes(e.target.value.toLowerCase()),
+    );
+    setFilteredSpares(filtered);
+  };
+
   return (
     <Box overflow="scroll" w="100%" {...rest}>
+      <Input onChange={handleFilter} placeholder="Buscar por tienda" />
       <Table size="sm" variant="simple">
         <Thead position="sticky" top={0}>
           <Tr>
@@ -64,52 +92,96 @@ export const SparesListing: React.FC<ISparesListingProps> = (
             ))}
           </Tr>
         </Thead>
-        {spares?.map((spare) => (
-          <Tbody key={spare?.id}>
-            <Tr position="relative">
-              <Td>{spare?.store}</Td>
-              <Td>{spare?.registrant_name}</Td>
-              <Td>{spare?.folio}</Td>
-              <Td
-                color={setColor(spare)}
-                minWidth="300px"
-                whiteSpace="pre-wrap"
-              >
-                {spare?.name}
-              </Td>
-              <Td>{spare?.unit}</Td>
-              <Td>{spare.store_manager ? spare.store_manager : 'S/D'}</Td>
-              <Td>{spare.supplier ? spare.supplier : 'S/D'}</Td>
-              <Td>
-                {spare.created_at
-                  ? new Date(spare?.created_at).toLocaleDateString()
-                  : 'S/D'}
-              </Td>
-              <Td>
-                {spare.request_date
-                  ? new Date(spare?.request_date).toLocaleDateString()
-                  : 'S/D'}
-              </Td>
-              <Td>
-                {spare.authorization_date
-                  ? new Date(spare?.authorization_date).toLocaleDateString()
-                  : 'S/D'}
-              </Td>
-              <Td>
-                {spare.reception_date
-                  ? new Date(spare?.reception_date).toLocaleDateString()
-                  : 'S/D'}
-              </Td>
-              <Td>
-                {spare.installation_date
-                  ? new Date(spare?.installation_date).toLocaleDateString()
-                  : 'S/D'}
-              </Td>
-              <Td>{spare.service_sheet ? 'Si' : 'S/D'}</Td>
-              <TdActions onUpdateSpare={() => handleOnUpdate(spare)} />
-            </Tr>
-          </Tbody>
-        ))}
+        {filteredSpares?.length !== undefined
+          ? filteredSpares?.map((spare) => (
+              <Tbody key={spare?.id}>
+                <Tr color={setColor(spare)} position="relative">
+                  <Td>{spare?.store}</Td>
+                  <Td>{spare?.registrant_name}</Td>
+                  <Td>{spare?.folio}</Td>
+                  <Td minWidth="300px" whiteSpace="pre-wrap">
+                    {spare?.name}
+                  </Td>
+                  <Td>{spare?.unit}</Td>
+                  <Td>{spare.store_manager ? spare.store_manager : 'S/D'}</Td>
+                  <Td>{spare.supplier ? spare.supplier : 'S/D'}</Td>
+                  <Td>
+                    {spare.created_at
+                      ? new Date(spare?.created_at).toLocaleDateString()
+                      : 'S/D'}
+                  </Td>
+                  <Td>
+                    {spare.request_date
+                      ? new Date(spare?.request_date).toLocaleDateString()
+                      : 'S/D'}
+                  </Td>
+                  <Td>
+                    {spare.authorization_date
+                      ? new Date(spare?.authorization_date).toLocaleDateString()
+                      : 'S/D'}
+                  </Td>
+                  <Td>
+                    {spare.reception_date
+                      ? new Date(spare?.reception_date).toLocaleDateString()
+                      : 'S/D'}
+                  </Td>
+                  <Td>
+                    {spare.installation_date
+                      ? new Date(spare?.installation_date).toLocaleDateString()
+                      : 'S/D'}
+                  </Td>
+                  <Td>{spare.service_sheet ? 'Si' : 'S/D'}</Td>
+                  <TdActions onUpdateSpare={() => handleOnUpdate(spare)} />
+                </Tr>
+              </Tbody>
+            ))
+          : spares?.map((spare) => (
+              <Tbody key={spare?.id}>
+                <Tr position="relative">
+                  <Td>{spare?.store}</Td>
+                  <Td>{spare?.registrant_name}</Td>
+                  <Td>{spare?.folio}</Td>
+                  <Td
+                    color={setColor(spare)}
+                    minWidth="300px"
+                    whiteSpace="pre-wrap"
+                  >
+                    {spare?.name}
+                  </Td>
+                  <Td>{spare?.unit}</Td>
+                  <Td>{spare.store_manager ? spare.store_manager : 'S/D'}</Td>
+                  <Td>{spare.supplier ? spare.supplier : 'S/D'}</Td>
+                  <Td>
+                    {spare.created_at
+                      ? new Date(spare?.created_at).toLocaleDateString()
+                      : 'S/D'}
+                  </Td>
+                  <Td>
+                    {spare.request_date
+                      ? new Date(spare?.request_date).toLocaleDateString()
+                      : 'S/D'}
+                  </Td>
+                  <Td>
+                    {spare.authorization_date
+                      ? new Date(spare?.authorization_date).toLocaleDateString()
+                      : 'S/D'}
+                  </Td>
+                  <Td>
+                    {spare.reception_date
+                      ? new Date(spare?.reception_date).toLocaleDateString()
+                      : 'S/D'}
+                  </Td>
+                  <Td>
+                    {spare.installation_date
+                      ? new Date(spare?.installation_date).toLocaleDateString()
+                      : 'S/D'}
+                  </Td>
+                  <Td>{spare.service_sheet ? 'Si' : 'S/D'}</Td>
+                  <TdActions onUpdateSpare={() => handleOnUpdate(spare)} />
+                </Tr>
+              </Tbody>
+            ))}
+
         <Thead position="sticky" top={0}>
           <Tr>
             {HEADERS.map((item) => (
